cf0b7a7d6eb9fa28b13d51b9a892a02c083a293e
teeaaspoon <tommypoon36@gmail.com>
2018-06-27 22:57:20 +0000

    finished registration

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
+  // hides the signup and login buttons
   if (document.cookie.length > 0) {
     $("#signup").css("display", "none");
     $("#login").css("display", "none");
   }
 
-  // make popup form
+  // make login form
   $("#login").on("click", function() {
-    $(".loginform").dialog("open");
+    $("#loginform").dialog("open");
   });
-  $(".loginform").dialog({
+  $("#loginform").dialog({
     autoOpen: false,
     modal: true
   });
-  $(".loginform form").on("submit", function(event) {
+  $("#loginform").on("submit", function(event) {
     event.preventDefault();
     const formSubmissionData = $(event.target);
     const email = formSubmissionData.children("input#email").val();
@@ -23,12 +24,42 @@ $(document).ready(function() {
       data: { email: email, password: password }
     }).then(function(response) {
       if (response === true) {
-        $(".loginform").dialog("close");
+        $("#loginform").dialog("close");
         document.cookie = "loggedin";
         $("#signup").css("display", "none");
         $("#login").css("display", "none");
       } else {
         window.alert("Invalid Login");
+      }
+    });
+  });
+
+  // signup form
+  $("#signup").on("click", function() {
+    $("#signupform").dialog("open");
+  });
+  $("#signupform").dialog({
+    autoOpen: false,
+    modal: true
+  });
+  $("#signupform").on("submit", function(event) {
+    event.preventDefault();
+    const formSubmissionData = $(event.target);
+    const email = formSubmissionData.children("input#email").val();
+    const password = formSubmissionData.children("input#password").val();
+    const username = formSubmissionData.children("input#username").val();
+    $.ajax({
+      url: "/register",
+      type: "POST",
+      data: { email: email, password: password, username: username }
+    }).then(function(response) {
+      if (response === true) {
+        $("#signupform").dialog("close");
+        document.cookie = "loggedin";
+        $("#signup").css("display", "none");
+        $("#login").css("display", "none");
+      } else {
+        window.alert("Email Or Username Already Exists");
       }
     });
   });
--- server/routes/users.js
+++ server/routes/users.js
@@ -9,7 +9,6 @@ module.exports = knex => {
   router.post("/login", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    console.log(password);
 
     // check if email exists in database
     knex("users")
@@ -35,5 +34,59 @@ module.exports = knex => {
       });
   });
 
+  router.post("/register", async (req, res) => {
+    const email = req.body.email;
+    const password = bcrypt.hashSync(req.body.password, 10);
+    const username = req.body.username;
+    // check if email already exists in database
+    const emailResult = await knex("users").where({ email: email });
+    if (emailResult[0]) {
+      console.log("Email already exists in database");
+      res.send(false);
+      return;
+    }
+    // check if username already exists in database
+    const usernameResult = await knex("users").where({ username: username });
+    if (usernameResult[0]) {
+      console.log("Username already exists in database");
+      res.send(false);
+      return;
+    }
+    // insert into database
+    knex("users")
+      .insert({ email: email, password: password, username: username })
+      .returning("id")
+      .then(result => {
+        req.session["user_id"] = result[0];
+        res.send(true);
+      });
+  });
+
   return router;
 };
+
+// // route to registration
+// router.post("/register", (req, res) => {
+//   const email = req.body.email;
+//   const password = bcrypt.hashSync(req.body.password, 10);
+//   const username = req.body.username;
+//   // check if email already exists in database
+//   knex("users")
+//   .where({ email: email })
+//   .then(function(result) {
+//     if (result || result[0]) {
+//       console.log("email already exists");
+//       res.send(false);
+//       return;
+//     }
+//   // check if username already exists in database
+//   knex("users")
+//   .where({ username: username })
+//   .then(function(result) {
+//     if (result || result[0]) {
+//       console.log("username already exists");
+//       res.send(false);
+//       return;
+//     }
+//   // insert into database
+// });
