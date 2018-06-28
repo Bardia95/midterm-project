c51e557ddd8bf234afbe8d7146cc39a38fef5dc2
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2018-06-28 12:26:54 -0400

    added profile page jquery/html skeleton

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,9 +5,11 @@ $(document).ready(function() {
     $("#login").css("display", "block");
     $("#logout").css("display", "none");
     $("#newpost").css("display", "none");
+    $("#my-profile").css("display", "none");
   } else if (document.cookie) {
     $("#logout").css("display", "block");
     $("#newpost").css("display", "block");
+    $("#my-profile").css("display", "block");
   }
   // closes signup/login/post box if clicked outside of element
 
@@ -150,5 +152,80 @@ $(document).ready(function() {
     }).then(result => {
       console.log("logged out");
     });
+  });
+
+
+  // Profile Page
+  $("#my-profile").on("click", function() {
+    $("main").empty();
+    let html = `
+      <section>
+        <img src=user.picture>
+        <div class="username">
+          <h2>user.username</h2>
+        </div>
+        <div class="userinfo">
+          <p>user.email</p> <button>Edit Email</button>
+          <button>Edit Password</button>
+        </div>
+        <input type="radio" name="toggle"> <!-- Toggle between upvoted and own posts -->
+        <div class="upposts">
+           <article class='post'>
+              <header>
+                <h2>post.title</h1>
+              </header>
+              <footer>
+                <p>post.description</p>
+                <div class='timeposted'>
+                  <p>post.date</p>
+                </div>
+                <div class='icons'>
+                  <i class='fas fa-chevron-up'></i>
+                  <p>Upvotes - Downvotes</p>
+                  <i class='fas fa-chevron-down'></i>
+                </div>
+              </footer>
+              <aside>
+                <div class="comments">
+                  <p>Comment 1</p>
+                  <p>user.username</p>
+                </div>
+                <form action="/post/comment" action="POST" id="comments-form">
+                  <textarea placeholder="Comment"></textarea>
+                  <input type="submit" id="comment-submit">
+                </form>
+              </aside>
+            </article>
+        </div>
+        <div class="ownposts">
+          <article class='post'>
+              <header>
+                <h2>post.title</h1>
+              </header>
+              <footer>
+                <p>post.description</p>
+                <div class='timeposted'>
+                  <p>post.date</p>
+                </div>
+                <div class='icons'>
+                  <i class='fas fa-chevron-up'></i>
+                  <p>Upvotes - Downvotes</p>
+                  <i class='fas fa-chevron-down'></i>
+                </div>
+              </footer>
+              <aside>
+                <div class="comments">
+                  <p>Comment 1</p>
+                  <p>user.username</p>
+                </div>
+                <form action="/post/comment" action="POST" id="comments-form">
+                  <textarea placeholder="Comment"></textarea>
+                  <input type="submit" id="comment-submit">
+                </form>
+              </aside>
+            </article>
+        </div>
+      </section>`;
+      $('main').append(html);
   });
 });
