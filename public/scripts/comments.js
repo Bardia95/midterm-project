03f547b34a59d638fd40a8edc2851db0c365a02c
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2018-06-28 20:00:19 -0400

    starting to fix css/script for comment

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,26 +1,27 @@
-// $(document).ready(function() {
-//   // comments
-//   $(".comment-form").on("submit", function(event) {
-//     event.preventDefault();
-//     const formSubmissionData = $(event.target);
-//     const content = formSubmissionData.children("textarea").val();
+$(document).ready(function() {
+  // comments
+  $("article").on("click", function(event) {
+    event.preventDefault();
+    const formSubmissionData = $(event.target);
+    const content = formSubmissionData.children("textarea").val();
+    const post_id = $(this).attr("postid");
+    console.log(post_id);
 
 
-//     $.ajax({
-//       url: "/post/:id/comment",
-//       type: "POST",
-//       data: {
-//         content: content,
-//         post_id:  1,
-//         user_id: 1
-//       }
-//     }).then(function(response) {
-//       if (response) {
-//         console.log("response: ", response);
-//       } else {
-//         window.alert("Invalid Comment");
-//       }
-//     });
-//   });
-// });
+    $.ajax({
+      url: "/post/:id/comment",
+      type: "POST",
+      data: {
+        content: content,
+        post_id:  post_id,
+      }
+    }).then(function(response) {
+      if (response) {
+        console.log("response: ", response);
+      } else {
+        window.alert("Invalid Comment");
+      }
+    });
+  });
+});
 
