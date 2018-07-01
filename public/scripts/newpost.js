d8b937f6569ee79b57afd434b19963d4822742bb
teeaaspoon <tommypoon36@gmail.com>
2018-07-01 00:41:06 +0000

    fixed new post to redirect to homepage when done

--- public/scripts/newpost.js
+++ public/scripts/newpost.js
@@ -43,60 +43,10 @@ $(document).ready(function() {
       if (response) {
         $("post").data("");
         $(".new-post").dialog("close");
-        renderPosts();
+        document.location.href = "/";
       } else {
         window.alert("Invalid Post");
       }
     });
   });
-  function renderPosts() {
-    $("main").empty();
-    // grab all the posts in the database
-    $.ajax({
-      url: "/render",
-      type: "GET"
-    }).then(result => {
-      // result is an array of post objects
-      result.forEach(post => {
-        $("main").prepend(createPostElement(post));
-      });
-    });
-  }
-  function createPostElement(postObject) {
-    const postID = postObject["id"];
-    const postTitle = postObject["title"];
-    const postDescription = postObject["description"];
-    const postDate = postObject["date_posted"];
-
-    return ` <article class='post rendered' data-postid=${postID}>
-    <header>
-      <h2>${postTitle}</h1>
-    </header>
-    <div class="post-description">
-      <p>${postDescription}</p>
-    </div>
-    <footer>
-      <div class='timeposted'>
-        <p>${moment(postDate).fromNow()}</p>
-      </div>
-      <div class='icons'>
-        <i class='fas fa-chevron-up'></i>
-        <p>2</p>
-        <i class='fas fa-chevron-down'></i>
-      </div>
-    </footer>
-    <aside>
-      <div class="comments">
-        <p>content</p>
-        <p>username</p>
-      </div>
-      <div class="comments-form">
-        <form action="/post/comment" method="POST" class="comment-form">
-          <textarea placeholder="comment" name="content"></textarea>
-          <input type="submit" name="comment-submit" class="comment-submit">
-        </form>
-      </div>
-    </aside>
-  </article>`;
-  }
 });
